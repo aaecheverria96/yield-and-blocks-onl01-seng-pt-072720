ddb2fe7fb515a4a5a3898103757c0beee36bb63f
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-06 03:49:32 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,5 @@
 def hello_t (array)
-  if block_given? 
+  if block_given?
   i = 0
 
 while i < array.length
@@ -8,7 +8,7 @@ while i < array.length
   end
 
 array
-else 
-  puts "Hey! No block was given!" 
-  end 
-end 
+else
+  puts "Hey! No block was given!"
+  end
+end
